a791a849807a0bd422379defb4caf34891537022
Andres Alonso Forero Pacheco <aforeroa@uninorte.edu.co>
2024-06-01 12:16:08 -0500

    Commit #1

---
+++ src/core/leaderboard/LeaderBoard.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core.leaderboard;
+
+import core.jugador.Jugador;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author andre
+ */
+public class LeaderBoard {
+    ArrayList<Jugador> leaderBoard = new ArrayList();
+}
